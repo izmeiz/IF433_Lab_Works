ce8ca7bbd1d745fb012283914fdb673e8487344f
izmeiz <164787492+izmeiz@users.noreply.github.com>
2026-02-12 11:44:30 +0700

    week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000103511_FaiizAprioPratama/Week1/CircleCalculator.kt
+++ src/oop_00000103511_FaiizAprioPratama/Week1/CircleCalculator.kt
@@ -1,15 +1,11 @@
 package oop_00000103511_FaiizAprioPratama.Week1
 
-fun main(args: Array<String>) {
-    // Variable definition
+fun main() {
     val radius = 7.0
     val pi = 3.14
-    // Calculation
     val area = pi * radius * radius
 
-    // Output Concatenation
     println("Radius: $radius, Area: $area" )
-    // REFACTOR
     println(checkSize(area))
 }
 fun checkSize(area: Double) = if (area > 100) "This is a Big Circle" else "This is a Small Circle"
