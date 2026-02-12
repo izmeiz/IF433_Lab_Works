d9881e6dce27285d88fd4504977774c686c63dde
izmeiz <164787492+izmeiz@users.noreply.github.com>
2026-02-12 13:03:05 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_00000103511_FaiizAprioPratama/week02/Student.kt
+++ src/oop_00000103511_FaiizAprioPratama/week02/Student.kt
@@ -1,6 +1,11 @@
 package oop_00000103511_FaiizAprioPratama.week02
 
-class Student(val name: String, val nim: String, var major: String) {
+class Student(
+    val name: String,
+    val nim: String,
+    var major: String,
+    var gpa: Double = 0.0
+) {
     init {
         // Cek panjang NIM
         if (nim.length != 5) {
