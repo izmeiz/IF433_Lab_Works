7db6e3f56674e444a2bc272a6b5863c0696f354c
izmeiz <164787492+izmeiz@users.noreply.github.com>
2026-02-12 12:04:21 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000103511_FaiizAprioPratama/Week1/GameStore.kt
+++ src/oop_00000103511_FaiizAprioPratama/Week1/GameStore.kt
@@ -1,11 +1,25 @@
 package oop_00000103511_FaiizAprioPratama.Week1
 
 fun main() {
-    val gameTitle: String = "Clair Obscur: Expedition 33"
-    val price: Int = 600000
+    val gameTitle  = "Clair Obscur: Expedition 33"
+    val price = 600000
+
+    //Hitung total harga
+    val discount = calculateDiscount(price)
+    val finalPrice = price - discount
+
+    printReceipt(title = gameTitle, finalPrice = finalPrice)
 
 }
 //Function implement discount
 fun calculateDiscount(price: Int): Int =
     if (price > 500000) (price * 0.2).toInt()
     else (price * 0.1).toInt()
+
+
+//Function Cetak Struk
+fun printReceipt(title: String, finalPrice: Int) {
+    println("===STRUK PEMBELIAN ===")
+    println("Judul Game: $title")
+    println("Harga Akhir : Rp $finalPrice")
+}
