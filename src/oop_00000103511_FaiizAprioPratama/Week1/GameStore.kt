a14b7929b7483c9ad8b33ad8e6aab359b1ee30a1
izmeiz <164787492+izmeiz@users.noreply.github.com>
2026-02-12 11:54:51 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000103511_FaiizAprioPratama/Week1/GameStore.kt
+++ src/oop_00000103511_FaiizAprioPratama/Week1/GameStore.kt
@@ -5,3 +5,7 @@ fun main() {
     val price: Int = 600000
 
 }
+//Function implement discount
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) (price * 0.2).toInt()
+    else (price * 0.1).toInt()
