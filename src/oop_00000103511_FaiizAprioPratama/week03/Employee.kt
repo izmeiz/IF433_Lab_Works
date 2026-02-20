fd3a2521c6cfc523d8f7c47f1c9b004ed8710f5c
izmeiz <164787492+izmeiz@users.noreply.github.com>
2026-02-20 15:04:54 +0700

    week03 : add private property

--- src/oop_00000103511_FaiizAprioPratama/week03/Employee.kt
+++ src/oop_00000103511_FaiizAprioPratama/week03/Employee.kt
@@ -1,6 +1,6 @@
 package oop_00000103511_FaiizAprioPratama.week03
 
-class Employee (val name: String){
+class Employee(val name: String){
     var salary: Int = 0
         set(value) {
             if (value < 0){
@@ -11,4 +11,14 @@ class Employee (val name: String){
             }
         }
 
+private var performanceRating: Int = 3
+
+    fun increasePerformance(){
+        performanceRating++
+        println("Kinerja $ meningkat! Rating: $performanceRating")
+    }
+
+    fun printStatus(){
+        println("Karyawan: $name, Rating: $performanceRating")
+    }
 }
