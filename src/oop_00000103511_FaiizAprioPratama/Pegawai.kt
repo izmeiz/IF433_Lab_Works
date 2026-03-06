6ee9011c22a957edee305f4c0cdcf412e7c248b8
izmeiz <164787492+izmeiz@users.noreply.github.com>
2026-03-06 10:40:09 +0700

    week05: create abstract class Pegawai

---
+++ src/oop_00000103511_FaiizAprioPratama/Pegawai.kt
@@ -0,0 +1,5 @@
+package oop_00000103511_FaiizAprioPratama
+
+abstract class Pegawai (val nama: String){
+    abstract fun bekerja ()
+}
---
+++ src/oop_00000103511_FaiizAprioPratama/Week05/Vehicle.kt
@@ -0,0 +1,4 @@
+package oop_00000103511_FaiizAprioPratama.Week05
+
+class Vehicle {
+}
