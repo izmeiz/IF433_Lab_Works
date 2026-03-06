a0799f383ed0d330582af06b9ce240bd2d0c84da
izmeiz <164787492+izmeiz@users.noreply.github.com>
2026-03-06 10:46:30 +0700

    week05: create Dosen class inheriting Pegawai

---
+++ src/oop_00000103511_FaiizAprioPratama/Week05/Dosen.kt
@@ -0,0 +1,11 @@
+package oop_00000103511_FaiizAprioPratama.Week05
+
+class Dosen (nama: String, val nidn : String) : oop_00000103511_FaiizAprioPratama.Pegawai(nama){
+    override fun bekerja() {
+        println("[$nama] sedang menyiapkan materi perkuliahan dan merevisi RPKPS.")
+    }
+
+    fun mengajar(){
+        println("[$nama] sedang mengajar mahasiswa di kelas.")
+    }
+}
