a0eba2282cdafdfddc4bbec6b2fa4a24a995f3a7
izmeiz <164787492+izmeiz@users.noreply.github.com>
2026-03-13 16:07:16 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000103511_FaiizAprioPratama/Week06/MainSmart.kt
+++ src/oop_00000103511_FaiizAprioPratama/Week06/MainSmart.kt
@@ -1,9 +1,21 @@
 package oop_00000103511_FaiizAprioPratama.Week06
 
-fun main() {
-    val lampuTamu = SmartLamp(id = "L-01", name = "Ruang Tamu")
+fun main () {
+    val smartHub = SmartHomeHub ()
+    val lampuTamu = SmartLamp("L-01", name = "Ruang Tamu")
+    val speakerDapur = SmartSpeaker ("S-01", name = "Google Nest Dapur")
+    val cctvGarasi = SmartCCTV ("C-01", name = "Ezviz Garasi")
 
-    val speakerDapur = SmartSpeaker(id = "S-01", name = "Google Nest Dapur")
+    println("==== INISIALISASI SISTEM =====")
+    smartHub.addDevice(lampuTamu)
+    smartHub.addDevice(speakerDapur)
+    smartHub.addDevice(cctvGarasi)
 
-    val cctvGarasi = SmartCCTV(id = "C-01", name = "Ezviz Garasi")
+    println("\n==== SIMULASI PENGGUNAAN HARIAN ====")
+    lampuTamu.turnOn()
+    speakerDapur.turnOn()
+    cctvGarasi.turnOn()
+
+    smartHub.activateSecurityMode()
+    smartHub.turnOffAllSwitches()
 }
