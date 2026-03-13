dd20c92dc202ad10330b993c7ea41693efa5d603
izmeiz <164787492+izmeiz@users.noreply.github.com>
2026-03-13 14:58:31 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000103511_FaiizAprioPratama/Week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000103511_FaiizAprioPratama.Week06
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.")}
+}
