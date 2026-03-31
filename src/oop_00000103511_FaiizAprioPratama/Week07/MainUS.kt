640a6d3052cb6d9254ea9b798275d799ca757fe3
izmeiz <faiizapriopratama12@gmail.com>
2026-03-31 18:16:06 +0700

    week07: resolve sealed class exhaustive check

--- src/oop_00000103511_FaiizAprioPratama/Week07/MainUS.kt
+++ src/oop_00000103511_FaiizAprioPratama/Week07/MainUS.kt
@@ -26,5 +26,8 @@ fun main() {
     val uiMessage = when(response) {
         is ApiResponse.Success -> "Tampilkan: ${response.data}"
         is ApiResponse.Error -> "Munculkan alert: ${response.message}"
+        is ApiResponse.Loading -> "Tampilkan Spinner"
     }
+
+    println("UI State: $uiMessage")
 }
