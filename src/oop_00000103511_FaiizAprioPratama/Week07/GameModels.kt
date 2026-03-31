bf23c243165aeabd6bab68bf60ab3947f858c4d2
izmeiz <faiizapriopratama12@gmail.com>
2026-03-31 18:26:03 +0700

    week07: (task) create ItemRarity enum

---
+++ src/oop_00000103511_FaiizAprioPratama/Week07/GameModels.kt
@@ -0,0 +1,9 @@
+package oop_00000103511_FaiizAprioPratama.Week07
+
+enum class ItemRarity (val dropChance: Int){
+    COMMON(70),
+    UNCOMMON(40),
+    RARE(15),
+    EPIC(5),
+    LEGENDARY(1)
+}
