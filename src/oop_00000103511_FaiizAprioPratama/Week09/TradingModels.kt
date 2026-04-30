f18e35fd95d24e124761d808cf514b7b1cf17ff9
izmeiz <faiizapriopratama12@gmail.com>
2026-04-30 18:42:36 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_00000103511_FaiizAprioPratama/Week09/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_00000103511_FaiizAprioPratama.Week09
+
+data class Tradelog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status : String
+)
