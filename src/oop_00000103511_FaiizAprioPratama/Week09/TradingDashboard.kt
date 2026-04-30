58807b2915811db456e31f954d60b43baef28959
izmeiz <faiizapriopratama12@gmail.com>
2026-04-30 18:48:59 +0700

    week09: (task) filter CLOSED trades only

--- src/oop_00000103511_FaiizAprioPratama/Week09/TradingDashboard.kt
+++ src/oop_00000103511_FaiizAprioPratama/Week09/TradingDashboard.kt
@@ -9,6 +9,8 @@ fun main(){
         TradeLog(pair = "ETHUSDT", position = "LONG", leverage = 25, roe = 8.4, status = "CLOSED"),
         TradeLog(pair = "BTCUSDT", position = "LONG", leverage = 50, roe = -12.0, status = "OPEN")
     )
-//Cetak Data
+
+    val closedTrades = tradeHistory.filter { it.status == "CLOSED" }
+
     println("Berhasil memuat ${tradeHistory.size} log perdagangan.")
 }
