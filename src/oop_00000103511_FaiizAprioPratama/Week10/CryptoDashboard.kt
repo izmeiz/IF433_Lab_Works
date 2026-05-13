08cdc73d136326dcc71d8d9a8f323dc3d77264c7
izmeiz <faiizapriopratama12@gmail.com>
2026-05-14 01:55:07 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000103511_FaiizAprioPratama/Week10/CryptoDashboard.kt
+++ src/oop_00000103511_FaiizAprioPratama/Week10/CryptoDashboard.kt
@@ -2,7 +2,10 @@ package oop_00000103511_FaiizAprioPratama.Week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+
     coinRepo.add(Coin("BTC", 0.125))
     coinRepo.add(Coin("ETH", 1.5))
     coinRepo.add(Coin("USDT", 500.0))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
