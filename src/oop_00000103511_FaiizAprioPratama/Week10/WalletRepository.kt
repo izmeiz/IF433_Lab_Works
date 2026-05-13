07c17e975f662ede79d1091d9a2a0ebd52410624
izmeiz <faiizapriopratama12@gmail.com>
2026-05-14 01:45:54 +0700

    week10: (task) implement search logic in repository

--- src/oop_00000103511_FaiizAprioPratama/Week10/WalletRepository.kt
+++ src/oop_00000103511_FaiizAprioPratama/Week10/WalletRepository.kt
@@ -1,13 +1,23 @@
 package oop_00000103511_FaiizAprioPratama.Week10
 
-class WalletRepository <T>{
+interface NamedItem {
+    val name: String
+}
+
+class WalletRepository<T : Any> {
     private val items = mutableListOf<T>()
 
-    fun add(item: T){
+    fun add(item: T) {
         items.add(item)
     }
 
-    fun getALL(): List<T>{
+    fun getAll(): List<T> {
         return items.toList()
+    }
+}
+
+fun <T : NamedItem> WalletRepository<T>.findByName(query: String): List<T> {
+    return getAll().filter {
+        it.name.contains(query, ignoreCase = true)
     }
 }
