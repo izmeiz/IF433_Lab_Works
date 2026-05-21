ca8e759173f75949b4529c3437756d3544ee3bf9
izmeiz <faiizapriopratama12@gmail.com>
2026-05-22 00:14:45 +0700

    week12: (task) handle feeding success using onSuccess

--- src/oop_00000103511_FaiizAprioPratama/Week12/SmartFeeder.kt
+++ src/oop_00000103511_FaiizAprioPratama/Week12/SmartFeeder.kt
@@ -41,7 +41,11 @@ fun main() {
             availableGram = 1000,
             isJammed = false
         )
+    }.onSuccess { newStock ->
+        currentKibbleStock = newStock
+        println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
     }
+
 }
 
 
